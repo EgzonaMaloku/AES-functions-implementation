cb0ae90e7f400d87a431d4621b6a84f67f6cdf82
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2024-11-06 13:55:16 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AesFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello AES");
+
+            Console.ReadKey();
+        }
+    }
+}
